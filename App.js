98508e05bf86e516f9ec914f1fd8cbdf83794184
aluno@ifpe <aluno@ifpe>
2024-02-22 20:35:33 -0300

    enviar

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Avatar } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
+import { Button } from 'react-native-elements';
+import { color } from 'react-native-elements/dist/helpers';
 
-export default function App() {
+
+const Flex = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+<View style={{alignItems:'center'}}>
+<Avatar 
+size={'large'}
+  rounded
+  source={{
+    uri:
+      'https://media.licdn.com/dms/image/D5603AQErPPCW2o7qrw/profile-displayphoto-shrink_200_200/0/1691414128167?e=2147483647&v=beta&t=R6g3MXVN0agZQCt0OYHk8a4wZsPNvIhYb4Io5g0oApU',
+  } }
+/>
 
-const styles = StyleSheet.create({
+<Input placeholder='E-mail'/>
+<Input placeholder="Password" secureTextEntry={true}/>
+<Button buttonStyle={{backgroundColor:"green"}} title="Logar" style={{marginTop:"5px"}}/>
+<Button buttonStyle={{backgroundColor:"green"}} title="Cadastre-se" style={{marginTop:"5px"} } />
+</View>
+  );
+  };
+  
+  const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  flex: 1,
+  padding: 20,
   },
-});
+  });
+  
+  //export default Flex;
+  
+
+
+  const Cadastro = () => {
+    return (
+  <View><Input placeholder='Nome'/>
+  <Input placeholder='E-mail'/>
+  <Input placeholder="Senha" secureTextEntry={true}/>
+  <Button buttonStyle={{backgroundColor:"green"}} title="Cadastrar"  style={{marginTop:"5px"} } />
+  </View>
+  );
+};
+
+export default Cadastro;
