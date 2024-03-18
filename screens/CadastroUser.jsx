544e3bdc580474b5f852d7150cbb0c03a122def3
Carlos Gabriel <carlosgmn31@gmail.com>
2024-03-17 23:27:52 -0300

    alert in on create user

--- screens/CadastroUser.jsx
+++ screens/CadastroUser.jsx
@@ -5,12 +5,14 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import app from "../firebase"
 import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
+import AlertPersonalizado from "../components/alertPersonalizado";
 
 const CadastroUser = () => {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [cpf, setCpf] = useState('');
   const [senha, setSenha] = useState('');
+  const [alertVisivel, setAlertVisivel] = useState(false);
 
   const auth = getAuth();
 
@@ -21,6 +23,7 @@ const CadastroUser = () => {
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
+        setAlertVisivel(true)
         SignedIn()
         
         // ...
@@ -33,7 +36,9 @@ const CadastroUser = () => {
       });
   };
   const navigation = useNavigation();
-
+  const hideAlert = () => {
+    setAlertVisivel(false);
+  };
   const SignedIn = () => {
     navigation.navigate('Lista');
   };
@@ -55,6 +60,11 @@ const CadastroUser = () => {
   <TextInput  value={senha}
             onChangeText={setSenha} style={styles.input}></TextInput>
   <Button  buttonStyle={{backgroundColor:"#3C6EF4", width: 250 }}  title="Salvar" style={{marginTop:"5px"}}  onPress={() => salvar()}/> 
+  <AlertPersonalizado 
+     visible={alertVisivel}
+     title="Parabéns!"
+     message="Cadastro realizado com sucesso"
+     onClose={hideAlert}/>
   </View>
   );
 };
